c90b89b2c11dc582fd20eb6556cd9eade3a1f4ff
Susan-Wangari <susanwangari810@gmail.com>
2021-03-12 05:54:25 -0800

    password validation

---
+++ password.py
@@ -0,0 +1,14 @@
+x = str(input('Please Enter username: '))
+y = str(input ('Enter password: '))
+username = "kay"
+password = "1234"
+
+if x == username :
+    print(username)
+else:
+    print("wrong username")
+
+    if y == password :
+        print(password)
+else:
+    print("wrong password")
--- test.py
+++ test.py
@@ -2,3 +2,7 @@ x = int(input('Enter X: '))
 y = int(input('Enter Y: '))
 sum = x + y
 print (sum)
+a = len('Susan Wangari')
+print(a)
+
+
